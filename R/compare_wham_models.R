59f4dd8a4e08d568668a35d5510c4efb4a4388f1
brianstock <bstock09@gmail.com>
2019-07-29 13:26:04 -0400

    added checks for valid model comparison

--- R/compare_wham_models.R
+++ R/compare_wham_models.R
@@ -7,6 +7,7 @@
 #' will be used.
 #' @param fname character, filename to save CSV results table (.csv will be appended). Default = "model_comparison".
 #' @param sort T/F, sort by AIC? Default = TRUE.
+#' @param mohns.rho T/F, calculate Mohn's rho? Retrospective analysis must have been run for all modes. Default = TRUE.
 #'
 #' @return a list with the following components:
 #'   \describe{
@@ -29,18 +30,42 @@
 #' }
 #'
 #' @export
-compare_wham_models <- function(mods, fname = "model_comparison", sort = TRUE){
+compare_wham_models <- function(mods, fname = "model_comparison", sort = TRUE, calc.rho = TRUE, calc.aic = TRUE){
   if(is.null(names(mods))) names(mods) <- paste0("m",1:length(mods))
-  aic <- sapply(mods, function(x){
-    2*(x$opt$obj + length(x$opt$par))
-  })
-  aic <- round(aic, 1)
-  rho <- t(sapply(mods, function(x){
-    mohns_rho(x)
-  }))[ ,c("R","SSB","Fbar")]
-  rho <- round(rho, 4)
-  colnames(rho) <- paste0("rho_",c("R","SSB","Fbar"))
-  # apply(rho, 1, function(y) mean(abs(y)))
+  aic <- NULL
+  if(calc.aic){
+    if(sum(mapply(function(x) x$env$data$Ecov_model==0, mods)) %in% c(0,length(mods))){
+      ecov.obs <- lapply(mods, function(x) x$env$data$Ecov_use_obs)
+      all.identical <- function(l) all(mapply(identical, head(l, 1), tail(l, -1)))
+      if(!all.identical(ecov.obs)){
+        stop("Different env covariate data used, cannot compare models' AIC.
+             Set 'calc.aic = FALSE' to compare only Mohn's rho, or only select
+             models fit to the same data.")
+      }
+      aic <- sapply(mods, function(x){
+        2*(x$opt$obj + length(x$opt$par))
+      })
+      aic <- round(aic, 1)
+    } else {
+      stop("Env covariate in some model(s) but not all. Cannot compare AIC
+           for models with different data (here, some have environmental data
+           and some do not).")
+    }
+  }
+  rho <- NULL
+  if(calc.rho){
+    if(any(mapply(function(x) is.null(x$peels), mods))){
+      stop("Not all models have peels --> Cannot compare Mohn's rho.
+           Set 'calc.rho = FALSE' to compare only AIC, or re-run models
+           with 'fit_wham(do.retro = TRUE)'.")
+    }
+    rho <- t(sapply(mods, function(x){
+      mohns_rho(x)
+    }))[ ,c("R","SSB","Fbar")]
+    rho <- round(rho, 4)
+    colnames(rho) <- paste0("rho_",c("R","SSB","Fbar"))
+    # apply(rho, 1, function(y) mean(abs(y)))
+  }
   tab <- cbind(aic, rho)
   colnames(tab) <- c("AIC", colnames(rho))
 
