745fab245712a7e672cffc9f5492685d3b5ec19e
brianstock <bstock09@gmail.com>
2019-10-21 12:06:58 -0400

    default n.newton=0 for projections

--- R/fit_wham.R
+++ R/fit_wham.R
@@ -30,7 +30,7 @@
 #'   Default: \code{osa.opts = list(method="oneStepGeneric", parallel=TRUE)}.
 #' @param model (optional), a previously fit wham model.
 #' @param do.check T/F, check if model parameters are identifiable? Passed to \code{\link{fit_tmb}}. Runs \code{\link[TMBhelper::Check_Identifiable]{TMBhelper::Check_Identifiable}}. Default = \code{TRUE}.
-#' @param do.proj T/F, do projections? Default = \code{TRUE}. Passed to \code{\link{fit_tmb}}. Runs \code{\link[TMBhelper::Check_Identifiable]{TMBhelper::Check_Identifiable}}. Default = \code{TRUE}.
+#' @param do.proj T/F, do projections? Default = \code{TRUE}. If true, runs \code{\link{project_wham}}.
 #' @param proj.opts list of options for projections
 #'   \itemize{
 #'     \item \code{$n.yrs} (integer), number of years to project/forecast. Default = \code{3}.
