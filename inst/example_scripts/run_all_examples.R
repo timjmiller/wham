9d5bb7cfcc60475033be5e74e1f867218e3bf8c9
brianstock-NOAA <brian.stock@noaa.gov>
2020-11-10 09:08:53 -0500

    fix file paths in run_all_examples.R

--- inst/example_scripts/run_all_examples.R
+++ inst/example_scripts/run_all_examples.R
@@ -1,46 +1,33 @@
 # Run all WHAM examples
 
 # choose where to save output, otherwise will be saved in working directory
-# write.dir <- "choose/where/to/save/output"
-if(!exists("write.dir")) write.dir = getwd()
-if(!dir.exists(write.dir)) dir.create(write.dir)
-setwd(write.dir)
+# main.dir <- "choose/where/to/save/output"
+if(!exists("main.dir")) main.dir = getwd()
+if(!dir.exists(main.dir)) dir.create(main.dir)
 
 library(wham)
 wham.dir <- find.package("wham")
 
 # Ex 1
-ex1.dir <- file.path(write.dir,"ex1")
-if(!dir.exists(ex1.dir)) dir.create(ex1.dir)
-setwd(ex1.dir)
+write.dir <- file.path(main.dir,"ex1")
 source(file.path(wham.dir, "example_scripts", "ex1_SNEMA_yellowtail_flounder.R"))
 
 # Ex 2
-ex2.dir <- file.path(write.dir,"ex2")
-if(!dir.exists(ex2.dir)) dir.create(ex2.dir)
-setwd(ex2.dir)
+write.dir <- file.path(main.dir,"ex2")
 source(file.path(wham.dir, "example_scripts", "ex2_CPI_recruitment_SNEMA_yellowtail.R"))
 
 # Ex 3
-ex3.dir <- file.path(write.dir,"ex3")
-if(!dir.exists(ex3.dir)) dir.create(ex3.dir)
-setwd(ex3.dir)
+write.dir <- file.path(main.dir,"ex3")
 source(file.path(wham.dir, "example_scripts", "ex3_projections.R"))
 
 # Ex 4
-ex4.dir <- file.path(write.dir,"ex4")
-if(!dir.exists(ex4.dir)) dir.create(ex4.dir)
-setwd(ex4.dir)
+write.dir <- file.path(main.dir,"ex4")
 source(file.path(wham.dir, "example_scripts", "ex4_selectivity.R"))
 
 # Ex 5
-ex5.dir <- file.path(write.dir,"ex5")
-if(!dir.exists(ex5.dir)) dir.create(ex5.dir)
-setwd(ex5.dir)
+write.dir <- file.path(main.dir,"ex5")
 source(file.path(wham.dir, "example_scripts", "ex5_M_GSI.R"))
 
 # Ex 6
-ex6.dir <- file.path(write.dir,"ex6")
-if(!dir.exists(ex6.dir)) dir.create(ex6.dir)
-setwd(ex6.dir)
+write.dir <- file.path(main.dir,"ex6")
 source(file.path(wham.dir, "example_scripts", "ex6_NAA.R"))
