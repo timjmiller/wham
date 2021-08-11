8f6bf305d162d6a3e3b90716cab6109db6557df1
brianstock-NOAA <brian.stock@noaa.gov>
2021-08-11 12:50:17 -0400

    tmb makeadfun checkparameterorder false

--- R/fit_wham.R
+++ R/fit_wham.R
@@ -88,7 +88,7 @@ fit_wham = function(input, n.newton = 3, do.sdrep = TRUE, do.retro = TRUE, n.pee
 
   # fit model
   if(missing(model)){
-    mod <- TMB::MakeADFun(input$data, input$par, DLL = "wham", random = input$random, map = input$map, silent = MakeADFun.silent)
+    mod <- TMB::MakeADFun(input$data, input$par, DLL = "wham", random = input$random, map = input$map, silent = MakeADFun.silent, checkParameterOrder=FALSE)
   } else {mod = model}
 
   mod$years <- input$years
