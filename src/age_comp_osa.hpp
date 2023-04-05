ae6288906bb110822ef9ec0acd03ebc8dcb52a07
Giancarlo H Morón Correa <g.moroncorrea@gmail.com>
2023-04-05 11:21:45 -0700

    multinom comps

--- src/age_comp_osa.hpp
+++ src/age_comp_osa.hpp
@@ -40,17 +40,21 @@ Type dmultinom(vector<Type> x, vector<Type> p, data_indicator<vector<Type>, Type
     //Type cdf;
     for(int i=0; i<x.size(); ++i){
       if(i!=(x.size()-1)){
-		vector<Type> x2(2), p2(2);
-		//Type one_minus_pUsed_i = squeeze(1.0-pUsed);
-		x2(0) = x(i);
-		x2(1) = nUnused-x(i);
-		p2(0) = squeeze(p(i)/(Type(1)-pUsed));//(Type(1)-pUsed_i); //for log of any p = 0
-		p2(1) = 1. - p2(0);
-		logres += k(i) * dmultinom(x2,p2,1); //binomial the hard way.
-		//logres += k(i)*dbinom(x(i),nUnused,p(i)/(Type(1)-pUsed),true);
-		//cdf = pbinom(x(i),nUnused,p(i)/(Type(1)-pUsed));
-		nUnused -= x(i);
-		pUsed += p(i);
+        vector<Type> x2(2), p2(2);
+        //Type p_i = squeeze(p(i));
+        //Type one_minus_pUsed_i = squeeze(1.0-pUsed);
+        x2(0) = x(i);
+        x2(1) = nUnused-x(i);
+        p2(0) = squeeze(p(i));
+        p2(0) /= squeeze((Type(1)-pUsed));
+        p2(0) = squeeze(p2(0));
+        // p2(0) = squeeze(p(i)/(Type(1)-pUsed));//(Type(1)-pUsed_i); //for log of any p = 0
+        p2(1) = 1. - p2(0);
+        logres += k(i) * dmultinom(x2,p2,1); //binomial the hard way.
+        //logres += k(i)*dbinom(x(i),nUnused,p(i)/(Type(1)-pUsed),true);
+        //cdf = pbinom(x(i),nUnused,p(i)/(Type(1)-pUsed));
+        nUnused -= x(i);
+        pUsed += p(i);
       }else{ // last index 
         logres += k(i)*Type(0);
         //cdf = Type(1);
@@ -62,7 +66,6 @@ Type dmultinom(vector<Type> x, vector<Type> p, data_indicator<vector<Type>, Type
   } else {
     logres = dmultinom(x,p,1);
   }
-  
   if(give_log){
     return logres;
   }else{ 
