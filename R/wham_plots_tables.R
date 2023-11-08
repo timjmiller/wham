008f92946730d2689354654181da7d149d1824c8
timjmiller <timothy.j.miller@noaa.gov>
2023-11-08 15:38:06 -0500

    edit some output plots

--- R/wham_plots_tables.R
+++ R/wham_plots_tables.R
@@ -209,7 +209,7 @@ plot.osa.residuals <- function(mod, do.tex=FALSE, do.png=FALSE, fontfam="", res=
       abline(h=-qnorm(0.975), col=plot.colors[f], lwd=2, lty = 2)
       abline(h=qnorm(0.975), col=plot.colors[f], lwd=2, lty = 2)
       # 4. trend vs. observed val
-      plot(vals, resids, type='p', col=plot.colors[f], pch=19, xlab="Observed proportion", ylab="OSA Residuals",
+      plot(vals, resids, type='p', col=plot.colors[f], pch=19, xlab="Observed", ylab="OSA Residuals",
            ylim=ylims)
       grid(lty = 2, col = "grey")
       abline(h=0, col=plot.colors[f], lwd=2)
@@ -368,7 +368,7 @@ plot.osa.residuals <- function(mod, do.tex=FALSE, do.png=FALSE, fontfam="", res=
       abline(h=-qnorm(0.975), col=plot.colors[i], lwd=2, lty = 2)
       abline(h=qnorm(0.975), col=plot.colors[i], lwd=2, lty = 2)
       # 4. trend vs. observed val
-      plot(vals, resids, type='p', col=plot.colors[i], pch=19, xlab="Observed proportion", ylab="OSA Residuals",
+      plot(vals, resids, type='p', col=plot.colors[i], pch=19, xlab="Observed", ylab="OSA Residuals",
            ylim=ylims)
       grid(lty = 2, col = "grey")
       abline(h=0, col=plot.colors[i], lwd=2)
@@ -434,8 +434,8 @@ plot.osa.residuals <- function(mod, do.tex=FALSE, do.png=FALSE, fontfam="", res=
            ylim=ylims)
       abline(h=0, col=plot.colors[f], lwd=2)
 
-      # 2. trend vs. fitted val
-      plot(tmp$pred, tmp$residual, type='p', col=plot.colors[f], pch=19, xlab=paste0("Predicted ", mod$input$Ecov_names[f]), ylab="OSA Residuals",
+      # 2. trend vs. observed
+      plot(tmp$val, tmp$residual, type='p', col=plot.colors[f], pch=19, xlab=paste0("Observed ", mod$input$Ecov_names[f]), ylab="OSA Residuals",
            ylim=ylims)
       abline(h=0, col=plot.colors[f], lwd=2)
 
@@ -2472,6 +2472,68 @@ get_YPR = function(F, M, sel, waacatch, at.age = FALSE)
   if(at.age) return(YPR)
   else return(sum(YPR))
 }
+
+#-------Y/R -----------------------------
+get_YPR_fleets = function(FAA,M, waacatch, at.age = FALSE)
+{
+  n_ages = dim(FAA)[2]
+  n_fleets <- dim(FAA)[1]
+  YPR = matrix(0,n_fleets, n_ages)
+  #F = F * sel
+  Z = apply(FAA,2,sum) + M
+  for(f in 1:n_fleets) {
+    n = 1
+    for(a in 1:(n_ages-1)){
+      YPR[f,a] <- n[a] * FAA[f,a] * waacatch[f,a] * (1.0 - exp(-Z[a]))/Z[a]
+      n[a+1] = n[a] * exp(-Z[a])
+    }
+    n[n_ages] = n[n_ages]/(1 - exp(-Z[n_ages]))
+    YPR[f,n_ages] = n[n_ages] * FAA[f,n_ages] * waacatch[f,n_ages] * (1.0 - exp(-Z[n_ages]))/Z[n_ages]
+  }
+  if(at.age) return(YPR) #by fleet and age
+  else return(apply(YPR,1,sum)) #by fleet
+}
+
+get_SPR_BRPS_fn <- function(mod, spr_yrs, percent){
+  dat = mod$env$data
+  if(missing(percent)) percent <- dat$percentSPR
+  if(missing(spr_yrs)) spr_yrs <- dat$avg_years_ind+1 #c++
+  R_yrs <- dat$XSPR_R_avg_yrs+1
+  R_type <- dat$XSPR_R_opt
+  if(R_type %in% c(1,3)) Rxspr <- mean(mod$rep$NAA[Ryrs,1])
+  else Rxspr <- mean(mod$rep$pred_NAA[R_yrs,1])
+  n_ages<- dat$n_ages
+  years <- mod$years
+  n_years <- length(years)
+  n_fleets <- dat$n_fleets
+
+  mat.age <- apply(dat$mature[spr_yrs,],2,mean)
+  ssb.waa <- apply(dat$waa[dat$waa_pointer_ssb,,][spr_yrs,],2,mean)
+  catch.waa <- apply(dat$waa[dat$waa_pointer_fleets,spr_yrs,,drop = FALSE],c(1,3),mean)
+  M.age <- apply(mod$rep$MAA[spr_yrs,],2,mean)
+  FAA <- apply(mod$rep$FAA[spr_yrs,,,drop=FALSE], 2:3, mean) #mean FAA by fleet
+  FAAtot = apply(FAA,2,sum) #sum across fleet averages 
+  seltot <- FAAtot/max(FAAtot)
+  selAA <- FAA/max(FAAtot) 
+  spawn.time <- mean(dat$fracyr_SSB[spr_yrs])
+  spr0 = get_SPR(F=0, M=M.age, sel=seltot, mat=mat.age, waassb=ssb.waa, fracyrssb = spawn.time)
+  F.start <- 0.11  # starting guess for optimization routine to find F_SPR%
+  spr.f <- function(F.start) {
+    spr = get_SPR(F=F.start, M=M.age, sel=seltot, mat=mat.age, waassb=ssb.waa, fracyrssb = spawn.time)
+    abs(100*spr/spr0 - percent)
+  }
+  opt <- nlminb(start=F.start, objective=spr.f, lower=0, upper=10)
+  Fxspr <- opt$par
+  spr_Fxspr <- get_SPR(Fxspr, M=M.age, sel=seltot, mat=mat.age, waassb=ssb.waa, fracyrssb = spawn.time)
+  FAA_xspr <- Fxspr * selAA
+  ypr_Fxspr <- get_YPR_fleets(FAA = FAA_xspr, M=M.age, waacatch= catch.waa)
+  Y_Fxspr <- Rxspr * ypr_Fxspr
+  SSB_Fxspr <- Rxspr*spr_Fxspr
+  return(list(Fxspr = Fxspr, FAA_xspr = FAA_xspr, SSB_Fxspr = SSB_Fxspr, Y_Fxspr = Y_Fxspr, spr_Fxspr = spr_Fxspr, Rxspr = Rxspr))
+}
+
+#y <- get_SPR_BRPS_fn(x)
+
 
 plot.SPR.table <- function(mod, nyrs.ave = 5, plot=TRUE)
 {
