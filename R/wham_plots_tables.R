3aa8da91dda0eb20b6e67c68336d4e5925858210
Emily Liljestrand <146266175+emilyliljestrand@users.noreply.github.com>
2026-08-31 19:24:19 +0000

    Midpoint always white regardless of color palette

--- R/wham_plots_tables.R
+++ R/wham_plots_tables.R
@@ -502,23 +502,10 @@ wham_palette <- function(n){
   )
 }
 
-wham_fill_scale <- function(...){
-  colors <- getOption("wham.colors", "default")
-
-  switch(
-    colors,
-    default = viridis::scale_fill_viridis(...),
-    cividis = viridis::scale_fill_viridis(option = "cividis", ...),
-    turbo = viridis::scale_fill_viridis(option = "turbo", begin = 0.2, end = 0.8, ...),
-    bw = ggplot2::scale_fill_gradient(low = "white", high = "black", ...),
-    gray = ggplot2::scale_fill_gradient(low = "#f2f2f2", high = "#666666", ...),
-    warm = ggplot2::scale_fill_gradientn(colours = grDevices::hcl.colors(256, "YlOrRd", rev = FALSE), ...),
-    ocean = ggplot2::scale_fill_gradientn(colours = grDevices::hcl.colors(256, "Teal", rev = FALSE), ...),
-    forest = ggplot2::scale_fill_gradientn(colours = grDevices::hcl.colors(256, "Green-Brown", rev = TRUE), ...),
-    highcontrast = ggplot2::scale_fill_gradientn(colours = grDevices::colorRampPalette(c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-      "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"), space = "Lab")(256), ...),
-    pastel = ggplot2::scale_fill_gradientn(colours = grDevices::hcl.colors(256, "Pastel 1", rev = FALSE), ...)
-  )
+wham_fill_scale <- function(midpoint, ...){
+  colors <- wham_palette(2)
+  ggplot2::scale_fill_gradient2(
+    low = colors[1], mid = "white", high = colors[2], midpoint = midpoint, ...)
 }
 
 fit.summary.text.plot.fn <- function(mod){
@@ -4321,6 +4308,8 @@ plot.tile.age.year <- function(mod, type="selAA", do.tex = FALSE, do.png = FALSE
     levels(df.plot$Age) <- ages.lab
     df.plot$Block <- factor(as.character(df.plot$Block), levels=block.names[include.selblock])
     fn <- "SelAA_tile"
+    sel.midpoint <- mean(range(df.plot$Selectivity, na.rm = TRUE))
+    if(!is.finite(sel.midpoint)) sel.midpoint <- 0
     if(do.tex) cairo_pdf(file.path(od, paste0(fn, ".pdf")), family = fontfam, height = 10, width = 10)
     if(do.png) png(filename = file.path(od, paste0(fn, ".png")), width = 10*144, height = 10*144, res = 144, pointsize = 12, family = fontfam)
       print(ggplot2::ggplot(df.plot, ggplot2::aes(x=Year, y=Age, fill=Selectivity)) + 
@@ -4330,7 +4319,7 @@ plot.tile.age.year <- function(mod, type="selAA", do.tex = FALSE, do.png = FALSE
 #        ggplot2::scale_y_continuous(expand=c(0,0), breaks = function(x) unique(floor(pretty(seq(0, (max(x) + 1) * 1.1))))) +        
         ggplot2::theme_bw() + 
         ggplot2::facet_wrap(~Block, dir="v") +
-        wham_fill_scale())
+        wham_fill_scale(midpoint = sel.midpoint))
     if(do.tex | do.png) dev.off()
   }
 
@@ -4361,6 +4350,8 @@ plot.tile.age.year <- function(mod, type="selAA", do.tex = FALSE, do.png = FALSE
     df.plot$Age <- as.factor(as.integer(df.plot$Age))
     levels(df.plot$Age) <- ages.lab
     fn <- paste0("MAA_tile")
+    M.midpoint <- mean(range(df.plot$M, na.rm = TRUE))
+    if(!is.finite(M.midpoint)) M.midpoint <- 0
 
     if(do.tex) cairo_pdf(file.path(od, paste0(fn, ".pdf")), family = fontfam, height = 5, width = 10)
     if(do.png) png(filename = file.path(od, paste0(fn, ".png")), width = 10*144, height = 5*144, res = 144, pointsize = 12, family = fontfam)
@@ -4371,7 +4362,7 @@ plot.tile.age.year <- function(mod, type="selAA", do.tex = FALSE, do.png = FALSE
       ggplot2::theme_bw() + 
       ggplot2::facet_grid(Stock~Region, labeller = ggplot2::label_both) +
       ggplot2::ggtitle("MAA") + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
-      wham_fill_scale()
+      wham_fill_scale(midpoint = M.midpoint)
       if(mod$env$data$n_years_proj>0) plt  <- plt + ggplot2::geom_vline(xintercept = tail(years,1), linetype = 2)
       print(plt)
     if(do.tex | do.png) dev.off()
@@ -4411,7 +4402,7 @@ plot.tile.age.year <- function(mod, type="selAA", do.tex = FALSE, do.png = FALSE
         ggplot2::theme_bw() + 
         ggplot2::facet_grid(Stock~Region, labeller = ggplot2::label_both) +
         ggplot2::ggtitle("NAA deviations") + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
-        wham_fill_scale(limits = c(-dev.limit, dev.limit))
+        wham_fill_scale(midpoint = 0, limits = c(-dev.limit, dev.limit))
       if(mod$env$data$n_years_proj>0) plt  <- plt + ggplot2::geom_vline(xintercept = tail(years,1), linetype = 2)
       print(plt)
     if(do.tex | do.png) dev.off()
